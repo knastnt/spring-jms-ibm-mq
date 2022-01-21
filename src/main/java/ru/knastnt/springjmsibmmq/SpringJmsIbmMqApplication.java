cad2150bc18080b6f35ee600d879c73d8b4ebb6b
knastnt <456kot@mail.ru>
2022-01-21 14:29:40 +1000

    init

---
+++ src/main/java/ru/knastnt/springjmsibmmq/SpringJmsIbmMqApplication.java
@@ -0,0 +1,13 @@
+package ru.knastnt.springjmsibmmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJmsIbmMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJmsIbmMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/knastnt/springjmsibmmq/SpringJmsIbmMqApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.knastnt.springjmsibmmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsIbmMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
